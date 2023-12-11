6c5568be4e268af23d46d7ffcf012de898be3443
YoungCoderboy <91416534+YoungCoderboy@users.noreply.github.com>
2023-12-11 13:04:27 +0530

    added mongoose in application

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const db = process.env.CONNECT.replace('<%PASSWORD%>', process.env.DB_PASSWORD);
+// console.log(db);
+mongoose.connect(db).then(connection => {
+  // console.log(connection);
+  console.log('Database connection established');
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
